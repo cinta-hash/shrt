9ecd846fa41fb9fa5532a05917d070b6bf8b1366
vboxuser <vboxuser@ubuntu.myguest.virtualbox.org>
2024-02-28 18:49:23 +0300

    changes

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,32 +1,38 @@
 class LinksController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_link, only: %i[ show edit update destroy ]
+  before_action :set_link, only: %i[show edit update destroy]
+  before_action :update_clicks, only: [:show]
+  
 
-  # GET /links or /links.json
   def index
-    @links = Link.all
+    @links = current_user ? current_user.links : []
   end
 
-  # GET /links/1 or /links/1.json
   def show
+    @link = current_user.links.find(params[:id])
+    @click_stats = @link.click_stats
+    redirect_to @link.long_url, allow_other_host: true
   end
 
-  # GET /links/new
   def new
     @link = Link.new
   end
 
-  # GET /links/1/edit
   def edit
   end
 
-  # POST /links or /links.json
   def create
-    @link = Link.new(link_params)
+    if current_user.nil?
+      flash[:error] = "Please log in to create a link."
+      redirect_to new_user_session_path
+      return
+    end
+    
+    @link = current_user.links.build(link_params)
 
     respond_to do |format|
       if @link.save
-        format.html { redirect_to link_url(@link), notice: "Link was successfully created." }
+        format.html { redirect_to link_url(@link), notice: 'Link was successfully created.' }
         format.json { render :show, status: :created, location: @link }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -35,11 +41,10 @@ class LinksController < ApplicationController
     end
   end
 
-  # PATCH/PUT /links/1 or /links/1.json
   def update
     respond_to do |format|
       if @link.update(link_params)
-        format.html { redirect_to link_url(@link), notice: "Link was successfully updated." }
+        format.html { redirect_to link_url(@link), notice: 'Link was successfully updated.' }
         format.json { render :show, status: :ok, location: @link }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -48,24 +53,43 @@ class LinksController < ApplicationController
     end
   end
 
-  # DELETE /links/1 or /links/1.json
   def destroy
     @link.destroy!
 
     respond_to do |format|
-      format.html { redirect_to links_url, notice: "Link was successfully destroyed." }
+      format.html { redirect_to links_url, notice: 'Link was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_link
-      @link = Link.find(params[:id])
+  def redirect_to_short_url
+    @link = Link.find_by_short_url(params[:id])
+    
+    if @link
+      @link.increment!(:clicks) # Increment the clicks count
+      redirect_to @link.long_url, allow_other_host: true
+    else
+      render json: { error: "Short URL not found" }, status: :not_found
     end
+  end
 
-    # Only allow a list of trusted parameters through.
-    def link_params
-      params.require(:link).permit(:short_url, :long_url, :custom_url, :clicks)
+  private
+
+  def set_link
+    @link = current_user.links.find_by(id: params[:id])
+    unless @link
+      flash[:error] = "Link not found"
+      redirect_to root_path
     end
+  end
+
+  def link_params
+    params.require(:link).permit(:long_url)
+  end
+
+  def update_clicks
+    return unless @link 
+
+    @link.increment!(:clicks)
+  end
 end
